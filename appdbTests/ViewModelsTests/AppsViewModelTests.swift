68e13ec5d91915997d103aaf480132e4eed0eae2
ned <github@edoardo.fyi>
2023-01-09 23:50:12 +0100

    added AppsViewModelTests

---
+++ appdbTests/ViewModelsTests/AppsViewModelTests.swift
@@ -0,0 +1,42 @@
+//
+//  AppsViewModelTests.swift
+//  appdbTests
+//
+//  Created by ned on 09/01/23.
+//
+
+import XCTest
+import Models
+@testable import appdb
+
+@MainActor final class AppsViewModelTests: XCTestCase {
+
+    func testAppsLoading() async throws {
+        let app = App(id: "1", name: "Example", image: .init(string: "https://edoardo.fyi/me.jpeg")!)
+        Dependencies.apiService.register {
+            .mock(.data([app]))
+        }
+        let viewModel = AppsView.ViewModel()
+        XCTAssertTrue(viewModel.state == .loading)
+        
+        await viewModel.loadApps()
+        
+        XCTAssertEqual(viewModel.state, .success([app]))
+    }
+    
+    func testAppsLoadingFail() async throws {
+        let apiError: APIError = .example
+        Dependencies.apiService.register {
+            .mock(.error(apiError))
+        }
+        let viewModel = AppsView.ViewModel()
+        XCTAssertTrue(viewModel.state == .loading)
+        
+        await viewModel.loadApps()
+        
+        XCTAssertEqual(viewModel.state, .failed(
+            APIError(code: apiError.code, translated: apiError.translated)
+        ))
+    }
+
+}
